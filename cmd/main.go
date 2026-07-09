53acf06f019db9c086117a01737bb1e3eaec7ea3
Bond, James Bond <mnailJk@gmail.com>
2026-07-09 10:48:20 +0300

    Add Morse Code & Decode functional

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
 
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
+)
+
+func main() {
+	filePath := "test"
+	filePath2 := "test12"
+
+	result, err := service.Run(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	result2, err := service.Run(filePath2)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(result)
+	fmt.Println(result2)
 }
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,56 @@
 package service
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+func GetFile(filePath string) (string, error) {
+	return filepath.Abs(filePath)
+}
+
+func ReadFile(filePath string) (string, error) {
+	fileData, err := os.ReadFile(filePath)
+	if err != nil {
+		return "", err
+	}
+
+	return string(fileData), nil
+}
+
+func DetectAndConvert(str string) (string, error) {
+	convertStr := strings.TrimSpace(str)
+
+	if convertStr == "" {
+		return "", errors.New("empty input")
+	}
+
+	isMorse := strings.IndexFunc(convertStr, func(r rune) bool {
+		return !strings.ContainsRune(".- /", r)
+	}) == -1
+
+	if isMorse {
+		return morse.ToText(convertStr), nil
+	}
+
+	return morse.ToMorse(convertStr), nil
+}
+
+func Run(filePath string) (string, error) {
+	absPath, err := GetFile(filePath)
+	if err != nil {
+		return "", fmt.Errorf("resolve path: %w", err)
+	}
+
+	content, err := ReadFile(absPath)
+	if err != nil {
+		return "", fmt.Errorf("read file: %w", err)
+	}
+
+	return DetectAndConvert(content)
+}
